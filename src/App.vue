5e27e70d56dfacd58243ca448939af3455bca871
keerthi-olm <keef-v@hotmail.com>
2018-12-10 17:02:37 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="app widget"><ToDoApp /></div>
+</template>
+
+<script>
+import ToDoApp from "./components/ToDoApp";
+
+export default {
+  name: "App",
+  components: {
+    ToDoApp
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/ToDoApp.vue
@@ -0,0 +1,206 @@
+<template>
+  <ul id="list">
+    <li class="header"><h2 class="title">My to do list</h2></li>
+    <li class="mainsmall">
+      <input ref="newItem" placeholder="Add a new task..." />
+    </li>
+    <li class="button add"><button v-on:click="add">Add</button></li>
+    <li class="button reset"><button onClick="{this.reset}">Reset</button></li>
+    <li class="main" v-for="(item, index) in list" :key="index">
+      <input
+        v-if="
+          doneList.filter(function(val) {
+            return val === index;
+          }).length === 0 ||
+            doneList.filter(function(val) {
+              return val === index;
+            }).length === undefined
+        "
+        class="checkbox"
+        type="checkbox"
+        :id="index"
+        v-on:click="handleClick"
+      />
+      <input
+        v-else
+        class="checkbox"
+        type="checkbox"
+        :checked="true"
+        :id="index"
+        v-on:click="handleClick"
+      />
+
+      <strike>{{ item }}</strike>
+    </li>
+
+    <li class="footer">
+      <button class="remove" v-on:click="remove">Remove</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ToDoApp",
+  data() {
+    return {
+      list: ["Get up in the morning", "Brush my teeth"],
+      doneList: [1,0]
+    };
+  },
+  computed: {
+    check(index) {
+      console.log("-------------->");
+      console.log(
+        this.doneList.filter(val => {
+          return val === index;
+        })
+      );
+      return (
+        this.doneList.filter(val => {
+          return false;
+        }) == undefined
+      );
+    }
+  },
+  methods: {
+    add() {
+      var newItem = this.$refs.newItem;
+      if (newItem.value === "") return;
+      this.list.push(newItem.value);
+      console.log(this.$refs.newItem.value);
+    },
+    handleClick(e) {
+      console.log(e.target.checked);
+      let checkIfInDoneList = this.doneList.filter(function(val) {
+        return val === parseInt(e.target.id);
+      });
+
+      if (checkIfInDoneList === undefined || checkIfInDoneList.length === 0) {
+        // add to list
+        this.doneList.push(parseInt(e.target.id));
+        e.target.checked = true;
+      } else {
+        //delete from list
+        let i = this.doneList.indexOf(parseInt(e.target.id));
+        this.doneList.splice(i, 1);
+        e.target.value = false;
+      }
+      console.log("donelist afterremove-->");
+      console.log(this.doneList);
+    },
+    remove(e) {
+      this.doneList.sort((a, b) => a - b);
+      console.log(this.doneList);
+      for (var i = this.doneList.length - 1; i >= 0; i--)
+        this.list.splice(this.doneList[i], 1);
+
+      console.log(this.list);
+      this.doneList = [];
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.App {
+  font-family: sans-serif;
+  text-align: center;
+  font-size: 1.5em;
+}
+
+.widget ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  height: 200px;
+
+  -ms-box-orient: horizontal;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -moz-flex;
+  display: -webkit-flex;
+  display: flex;
+
+  -webkit-justify-content: space-around;
+  justify-content: space-around;
+  -webkit-flex-flow: row wrap;
+  flex-flow: row wrap;
+  -webkit-align-items: stretch;
+  align-items: stretch;
+}
+
+.header,
+.footer,
+.main {
+  flex: 1 100%;
+}
+.button {
+  flex: 1;
+}
+.mainsmall {
+  flex: 9;
+}
+
+.widget li {
+  /*background: tomato;*/
+  padding: 10px;
+  width: 500px;
+  // border: 3px solid rgba(0, 0, 0, 0.2);
+
+  /*color: white;*/
+  font-weight: bold;
+  font-size: 0.8em;
+  text-align: right;
+}
+
+.widget li.header,
+.widget li.footer {
+  text-align: center;
+}
+
+button {
+  color: #fff;
+  text-transform: uppercase;
+  background: #ed3330;
+  padding: 0.5vw;
+  border-radius: 5px;
+  display: inline-block;
+  border: none;
+  height: 100%;
+}
+button.remove {
+  background-color: green;
+  padding: 0.1em;
+  height: ;
+}
+
+.widget li.mainsmall input {
+  width: 100%;
+  height: 100%;
+}
+.widget li.main {
+  font-size: 1.1em;
+  line-height: 1.2em;
+  display: flex;
+  align-items: center;
+}
+
+.widget .checkbox {
+  height: 1em;
+  width: 1em;
+  color: green;
+  margin-right: 0.2em;
+}
+
+strike {
+  text-decoration: none;
+}
+
+input:checked + strike {
+  text-decoration: line-through;
+  opacity: 0.5;
+}
+</style>
